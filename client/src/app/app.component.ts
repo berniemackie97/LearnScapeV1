d645b990e65e1207de8f40fa6b3b176428b362ac
berniemackie97 <berniemack97@icloud.com>
2023-01-09 15:40:53 -0500

    added interface with API and front end, added pagination to frontend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { IPagination } from "./models/pagination";
+import { IProduct } from "./models/products";
+
+export function toggleMobileMenu() {
+    console.log("toggleMobileMenu() called");
+    const icon = document.getElementById("hamburger-icon");
+    console.log(icon.classList);
+    icon.classList.toggle("open");
+    const menu = document.querySelector(".mobile-menu");
+    console.log(menu.classList);
+}
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = "LearnScape";
     faCoffee = faCoffee;
+    products: IProduct[];
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        this.http
+            .get("https://localhost:5001/api/products?pageSize=50")
+            .subscribe(
+                (response: IPagination) => {
+                    this.products = response.data;
+                    console.log(response);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FontAwesomeModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, NavBarComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        FontAwesomeModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule,
+        MatIconModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
